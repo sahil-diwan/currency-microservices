af17b63c1e4eeb5450e6fc1ad2332f6c2355c8ee
sahil-diwan <isahildiwan101@gmail.com>
2021-03-28 16:45:20 +0530

    Initial Application setup using the H2 Connection is completed.

---
+++ currency-exchange-service/src/main/java/com/sahildiwan/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,11 @@
+package com.sahildiwan.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long>{
+	
+	CurrencyExchange findByFromAndTo(String from,String to);
+
+}
