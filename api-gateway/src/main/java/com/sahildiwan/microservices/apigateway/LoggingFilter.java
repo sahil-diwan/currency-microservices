2ec7893d36f2d9ca991a167069c57173d7dea024
sahil-diwan <isahildiwan101@gmail.com>
2021-03-31 18:51:05 +0530

    Add the route related custom filters as well as the global filters

---
+++ api-gateway/src/main/java/com/sahildiwan/microservices/apigateway/LoggingFilter.java
@@ -0,0 +1,18 @@
+package com.sahildiwan.microservices.apigateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+
+public class LoggingFilter implements GlobalFilter{
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
